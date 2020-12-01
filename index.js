f9b864a55b6bd7cf05917814aab80d0c5af00513
Rohit Gaur <rohitgaur7@gmail.com>
2020-12-01 07:25:13 +0000

    refactor: moved config to .env

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ let log = console.log
 // Firebase configuration - this should not be public.
 // Please use your own config if you intend to use this code.
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
 }
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
